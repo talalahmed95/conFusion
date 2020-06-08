05ee522815e059b67e68cc7268d40a98cabca5dc
Talal <tahmed530@gmail.com>
2020-06-09 01:48:59 +0500

    gulp part-2

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,16 @@
 
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
-	browserSync = require('browser-sync');
+	browserSync = require('browser-sync'),
+	del = require('del'),
+	imagemin = require('gulp-imagemin'),
+	uglify = require('gulp-uglify'),
+	usemin = require('gulp-usemin'),
+	rev = require('gulp-rev'),
+	cleanCss = require('gulp-clean-css'),
+	flatmap = require('gulp-flatmap'),
+	htmlmin = require('gulp-htmlmin');
+
 
 gulp.task('sass', function(){
 	return gulp.src('./css/*.scss')
@@ -11,7 +20,7 @@ gulp.task('sass', function(){
 });
 
 gulp.task('sass:watch', function(){
-	gulp.watch('./css/*.scss', ['sass']);
+	gulp.watch('./css/*.scss', gulp.series('sass'));
 });
 
 gulp.task('browser-sync', function(){
@@ -29,10 +38,58 @@ gulp.task('browser-sync', function(){
 	});
 });
 
+// Clean
+gulp.task('clean', function() {
+	return del(['dist']);
+});
+
+// Copy fonts
+gulp.task('copyfonts', function() {
+	return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,eof,svg,woff}')
+	.pipe(gulp.dest('./dist/fonts'));
+});
+
+// Images
+gulp.task('imagemin', function() {
+	return gulp.src('./img/*.{png,jpg,jpeg,gif}')
+	.pipe(imagemin({
+		optimizationLevel: 3,
+		progressive: true,
+		interlaced: true
+	}))
+	.pipe(gulp.dest('./dist/img'));
+});
+
+// Usemin
+gulp.task('usemin', function() {
+	return gulp.src('./*.html')
+	.pipe(flatmap(function(stream, file) {
+		return stream
+		.pipe(usemin({
+			css: [ rev() ],
+			html: [ function() {
+				return htmlmin({
+					collapseWhitespace: true
+				})
+			}],
+			js: [ uglify(), rev() ],
+			inlinejs: [ uglify() ],
+			inlinecss: [ cleanCss(), 'concat' ]
+		}))
+	}))
+	.pipe(gulp.dest('./dist/'));
+});
+
+
+
 // Default task
+// gulp 3.9.1 - Error in following code, can't use gulp 3 with node 12
 
 // gulp.task('default', ['browser-sync'], function(){
 // 	gulp.start('sass:watch');
 // });
 
-gulp.task('default', gulp.series('browser-sync', 'sass:watch'));
+// gulp 4.0.2 - replaced prev code with gulp.series() to stop AssertionError
+gulp.task('default', gulp.parallel('browser-sync', 'sass:watch'));
+
+gulp.task('build', gulp.series('clean', gulp.parallel('copyfonts', 'imagemin', 'usemin')));
